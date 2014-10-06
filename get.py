efefc5c0d4da3abcb4080639fea4087ecdf579a9
Dheeraj Sayala <dheerosaur@gmail.com>
2014-10-06 18:00:42 +0530

    Very first version of get.py

---
+++ get.py
@@ -0,0 +1,27 @@
+import csv
+import json
+
+KEYS = (
+    'vendor_id', 'rate_code',
+    'pickup_datetime', 'dropoff_datetime',
+    'trip_time_in_secs', 'trip_distance',
+    'pickup_longitude', 'pickup_latitude',
+    'dropoff_longitude', 'dropoff_latitude',
+)
+
+
+def extract_data(fname):
+    trips = []
+    with open(fname, 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            trip = {k: row[k] for k in KEYS}
+            trips.append(trip)
+    return trips
+
+
+if __name__ == '__main__':
+    data = extract_data('test.csv')
+    json_data = json.dumps(data)
+    with open('trips.json', 'w+') as f:
+        f.write(json_data)
