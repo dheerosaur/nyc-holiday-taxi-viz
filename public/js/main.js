64abb736e6a049061c2d5d7c97c48940901dd706
Dheeraj Sayala <dheerosaur@gmail.com>
2014-10-14 00:29:49 +0530

    Add submit button and repaint polylines on submit

--- public/js/main.js
+++ public/js/main.js
@@ -33,111 +33,135 @@ var TERMINAL_COLORS = {
   'EWR': '#099',
 }
 
-d3.json('/trip', function (data) {
-  var options =  {weight: 2, opacity: .8}
+var polylines = [];
 
-  _.each(data, function (trip) {
-    options.color = getLineColor(trip.terminal);
-    L.Polyline.fromEncoded(trip.direction, options).addTo(map);
+function updateQuery () {
+
+  _.each(polylines, function (l) {
+    map.removeLayer(l);
   });
 
-  function getLineColor(terminal) {
-    return TERMINAL_COLORS[terminal.substring(0, 3)];
-  }
+  d3.json('/trip', function (data) {
+    var options =  {weight: 2, opacity: .8}
 
-  /*
-  var feature = g.selectAll('path')
-    .data(data.features)
-    .enter().append('path');
+    _.each(data, function (trip) {
+      options.color = getLineColor(trip.terminal);
+      var polyline = L.Polyline.fromEncoded(trip.direction, options).addTo(map);
+      polylines.push(polyline);
+    });
 
-  var pointsArray = [];
-  var points = g.selectAll('.point')
-    .data(pointsArray);
-
-  var marker = g.append('circle');
-  marker.attr('r', 5).attr('id', 'marker');
-
-  function iterate() {
-    var path = svg.select('path.trip' + i)
-      .attr('style', 'opacity:.7')
-      .call(transition);
-
-    function pathStartPoint(path) {
-      var d = path.attr('d');
-      dsplitted = d.split('L')[0].slice(1).split(',');
-      return [parseInt(dsplitted[0]), parseInt(dsplitted[1])];
+    function getLineColor(terminal) {
+      return TERMINAL_COLORS[terminal.substring(0, 3)];
     }
 
-    var startPoint = pathStartPoint(path);
-    marker.attr('transform', 'translate(' + startPoint[0] + ',' + startPoint[1] + ')');
+    /*
+    var feature = g.selectAll('path')
+      .data(data.features)
+      .enter().append('path');
 
-  }
+    var pointsArray = [];
+    var points = g.selectAll('.point')
+      .data(pointsArray);
 
-  function transition(path) {
-    g.selectAll
-    path.transition()
-      .duration(function (d) {
-        var start = Date.parse(d.properties.pickup_datetime)
-          , finish = Date.parse(d.properties.dropofftime)
-          , duration = (finish - start) / 60000;
+    var marker = g.append('circle');
+    marker.attr('r', 5).attr('id', 'marker');
 
-        duration = duratinon / timeFactor * 1000
-        return (duration);
-      })
-      .attrTween('stroke-dasharray', tweenDash);
-  }
+    function iterate() {
+      var path = svg.select('path.trip' + i)
+        .attr('style', 'opacity:.7')
+        .call(transition);
 
-  function tweenDash(d) {
-    var l = path.node().getTotalLength();
-    var i = d3.interpolateString('0,' + l, l + ',' + l);
-    return function (t) {
-      var marker = d3.select('#marker');
-      var p = path.node().getPointAtLength(t * l);
-      marker.attr('transform', 'translate(' + p.x + ',' + p.y + ')');
-      if (tweenToggle == 0) {
-        tweenToggle = 1;
-        var newCenter = map.layerPointToLatLng(new L.Point(p.x, p.y));
-      } else {
-        tweenToggle = 0;
+      function pathStartPoint(path) {
+        var d = path.attr('d');
+        dsplitted = d.split('L')[0].slice(1).split(',');
+        return [parseInt(dsplitted[0]), parseInt(dsplitted[1])];
       }
-      return i(t);
-    };
-  }
+
+      var startPoint = pathStartPoint(path);
+      marker.attr('transform', 'translate(' + startPoint[0] + ',' + startPoint[1] + ')');
+
+    }
+
+    function transition(path) {
+      g.selectAll
+      path.transition()
+        .duration(function (d) {
+          var start = Date.parse(d.properties.pickup_datetime)
+            , finish = Date.parse(d.properties.dropofftime)
+            , duration = (finish - start) / 60000;
+
+          duration = duratinon / timeFactor * 1000
+          return (duration);
+        })
+        .attrTween('stroke-dasharray', tweenDash);
+    }
+
+    function tweenDash(d) {
+      var l = path.node().getTotalLength();
+      var i = d3.interpolateString('0,' + l, l + ',' + l);
+      return function (t) {
+        var marker = d3.select('#marker');
+        var p = path.node().getPointAtLength(t * l);
+        marker.attr('transform', 'translate(' + p.x + ',' + p.y + ')');
+        if (tweenToggle == 0) {
+          tweenToggle = 1;
+          var newCenter = map.layerPointToLatLng(new L.Point(p.x, p.y));
+        } else {
+          tweenToggle = 0;
+        }
+        return i(t);
+      };
+    }
 
 
-  map.on('viewreset', reset);
-  reset();
+    map.on('viewreset', reset);
+    reset();
 
-  function reset() {
-    var bounds = d3path.bounds(data);
-    topLeft = bounds[0],
-    bottomRight = bounds[1];
+    function reset() {
+      var bounds = d3path.bounds(data);
+      topLeft = bounds[0],
+      bottomRight = bounds[1];
 
-    svg.attr("width", bottomRight[0] - topLeft[0] + 100)
-    .attr("height", bottomRight[1] - topLeft[1] + 100)
-    .style("left", topLeft[0]-50 + "px")
-    .style("top", topLeft[1]-50 + "px");
+      svg.attr("width", bottomRight[0] - topLeft[0] + 100)
+      .attr("height", bottomRight[1] - topLeft[1] + 100)
+      .style("left", topLeft[0]-50 + "px")
+      .style("top", topLeft[1]-50 + "px");
 
-    g.attr("transform", "translate(" + (-topLeft[0]+50) + "," + (-topLeft[1]+50)+ ")");
+      g.attr("transform", "translate(" + (-topLeft[0]+50) + "," + (-topLeft[1]+50)+ ")");
 
-    feature.attr("d", d3path);
+      feature.attr("d", d3path);
 
-    //TODO: Figure out why this doesn't work as points.attr...
-    g.selectAll(".point")
-    .attr("transform",function(d){
-      return translatePoint(d);
-    });
-  }
-  */
-});
+      //TODO: Figure out why this doesn't work as points.attr...
+      g.selectAll(".point")
+      .attr("transform",function(d){
+        return translatePoint(d);
+      });
+    }
+    */
+  });
 
+}
+
+updateQuery();
 
 $(function () {
 
   $('.airports input').change(function () {
     var airport = $(this).val();
     $('.terminals').hide();
+    $('.terminals input').removeAttr('checked');
     $('#' + airport + '-terminals').show();
+  });
+
+  $('.filters .submit').click(function () {
+    var airport = $('input[name=airport]:checked').val()
+      , terminal = $('.terminals input:checked').val();
+    $(this).css('color', 'black');
+    if (airport && terminal) {
+      updateQuery();
+    } else {
+      $(this).css('color', '#d33030');
+    }
   });
 
 });
