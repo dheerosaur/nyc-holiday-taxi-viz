1a7ea94b67a2b81b4d93d6bafa1549be72f62aae
Dheeraj Sayala <dheerosaur@gmail.com>
2014-10-16 01:55:27 +0530

    Implement map polylines using d3.js

--- public/js/main.js
+++ public/js/main.js
@@ -27,120 +27,65 @@ function projectPoint (x, y) {
 var transform = d3.geo.transform({ point: projectPoint })
   , d3path = d3.geo.path().projection(transform);
 
-var TERMINAL_COLORS = {
-  'JFK': '#3f9',
-  'LGA': '#fc0',
-  'EWR': '#099',
-}
+var options =  {weight: 2, opacity: .8}
 
-var polylines = [];
+function getLineFeature (trip, index) {
+  var coordinates = L.PolylineUtil.decode(trip.direction);
+  return {
+    type: 'Feature',
+    properties: {
+      key: index,
+      terminal: trip.terminal,
+      passenters: trip.passenger_count
+    },
+    geometry: {
+      type: 'LineString',
+      coordinates: _.map(coordinates, function (c) {
+        return [c[1], c[0]];
+      })
+    }
+  }
+}
 
 function updateQuery (query) {
   if (_.isUndefined(query)) query = {};
-
-  _.each(polylines, function (l) {
-    map.removeLayer(l);
-  });
-
   var url = '/trip?' + $.param(query);
 
-  d3.json(url, function (data) {
-    var options =  {weight: 2, opacity: .8}
+  d3.json(url, function (rawData) {
 
-    _.each(data, function (trip) {
-      options.color = getLineColor(trip.terminal);
-      var polyline = L.Polyline.fromEncoded(trip.direction, options).addTo(map);
-      polylines.push(polyline);
-    });
+    var data = {
+      type: 'FeatureCollection',
+      features: _.map(rawData, getLineFeature)
+    };
 
-    function getLineColor(terminal) {
-      return TERMINAL_COLORS[terminal.substring(0, 3)];
-    }
+    g.selectAll('path').remove();
 
-    /*
     var feature = g.selectAll('path')
       .data(data.features)
-      .enter().append('path');
-
-    var pointsArray = [];
-    var points = g.selectAll('.point')
-      .data(pointsArray);
-
-    var marker = g.append('circle');
-    marker.attr('r', 5).attr('id', 'marker');
-
-    function iterate() {
-      var path = svg.select('path.trip' + i)
-        .attr('style', 'opacity:.7')
-        .call(transition);
-
-      function pathStartPoint(path) {
-        var d = path.attr('d');
-        dsplitted = d.split('L')[0].slice(1).split(',');
-        return [parseInt(dsplitted[0]), parseInt(dsplitted[1])];
-      }
-
-      var startPoint = pathStartPoint(path);
-      marker.attr('transform', 'translate(' + startPoint[0] + ',' + startPoint[1] + ')');
-
-    }
-
-    function transition(path) {
-      g.selectAll
-      path.transition()
-        .duration(function (d) {
-          var start = Date.parse(d.properties.pickup_datetime)
-            , finish = Date.parse(d.properties.dropofftime)
-            , duration = (finish - start) / 60000;
-
-          duration = duratinon / timeFactor * 1000
-          return (duration);
-        })
-        .attrTween('stroke-dasharray', tweenDash);
-    }
-
-    function tweenDash(d) {
-      var l = path.node().getTotalLength();
-      var i = d3.interpolateString('0,' + l, l + ',' + l);
-      return function (t) {
-        var marker = d3.select('#marker');
-        var p = path.node().getPointAtLength(t * l);
-        marker.attr('transform', 'translate(' + p.x + ',' + p.y + ')');
-        if (tweenToggle == 0) {
-          tweenToggle = 1;
-          var newCenter = map.layerPointToLatLng(new L.Point(p.x, p.y));
-        } else {
-          tweenToggle = 0;
-        }
-        return i(t);
-      };
-    }
-
+      .enter().append('path')
+      .style('opacity', 1)
+      .attr('class', function (d) {
+        return ('trip-' + d.properties.key) + ' ' +
+               ('from-' + d.properties.terminal.slice(0, 3));
+      });
 
     map.on('viewreset', reset);
     reset();
 
-    function reset() {
-      var bounds = d3path.bounds(data);
-      topLeft = bounds[0],
-      bottomRight = bounds[1];
+    function reset () {
+      var bounds = d3path.bounds(data)
+        , topLeft = bounds[0]
+        , bottomRight = bounds[1];
 
       svg.attr("width", bottomRight[0] - topLeft[0] + 100)
-      .attr("height", bottomRight[1] - topLeft[1] + 100)
-      .style("left", topLeft[0]-50 + "px")
-      .style("top", topLeft[1]-50 + "px");
+        .attr("height", bottomRight[1] - topLeft[1] + 100)
+        .style("left", topLeft[0] - 50 + "px")
+        .style("top", topLeft[1] - 50 + "px");
 
       g.attr("transform", "translate(" + (-topLeft[0]+50) + "," + (-topLeft[1]+50)+ ")");
 
       feature.attr("d", d3path);
-
-      //TODO: Figure out why this doesn't work as points.attr...
-      g.selectAll(".point")
-      .attr("transform",function(d){
-        return translatePoint(d);
-      });
     }
-    */
   });
 
 }
