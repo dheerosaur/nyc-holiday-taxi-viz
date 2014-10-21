46cc2b05deb73939e8182d8f847dc039485087c5
Dheeraj Sayala <dheerosaur@gmail.com>
2014-10-21 23:54:53 +0530

    Move initialization code into functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,4 @@
-var map = L.map('map', {zoomControl: false}).setView([40.708, -73.954], 12);
-
-var TOKEN = '?access_token=pk.eyJ1IjoiZGhlZXJvc2F1ciIsImEiOiJKdHQ2TTJZIn0.qlhdcUlB-i7vnDaXgkNxhw'
-  , MAPID = 'dheerosaur.jo9f69e5'
-
-L.tileLayer('http://{s}.tiles.mapbox.com/v4/' + MAPID + '/{z}/{x}/{y}.png' + TOKEN, {
-  attribution: 'Copyright',
-  maxZoom: 18,
-}).addTo(map);
-
-L.control.zoom({ position: 'topright' }).addTo(map);
-
-
-var svg = d3.select(map.getPanes().overlayPane).append("svg")
-  , g = svg.append("g").attr("class", "leaflet-zoom-hide")
-  , transform = d3.geo.transform({ point: projectPoint })
-  , d3path = d3.geo.path().projection(transform);
-
-/* Functions */
+/* Utility functions */
 
   function translatePoint(d) {
     var point = map.latLngToLayerPoint(new L.LatLng(d[1],d[0]));
@@ -39,6 +21,32 @@ var svg = d3.select(map.getPanes().overlayPane).append("svg")
   }
 
 /* End utility functions */
+
+var map, svg, g, transform, d3path;
+
+function initMap () {
+  map = L.map('map', {zoomControl: false}).setView([40.708, -73.954], 12);
+
+  var MAP_ROOT = 'http://{s}.tiles.mapbox.com/v4/dheerosaur.jo9f69e5'
+    , TOKEN = 'pk.eyJ1IjoiZGhlZXJvc2F1ciIsImEiOiJKdHQ2TTJZIn0.qlhdcUlB-i7vnDaXgkNxhw';
+
+  L.tileLayer(MAP_ROOT + '/{z}/{x}/{y}.png?access_token=' + TOKEN, {
+    attribution: 'Copyright',
+    maxZoom: 18,
+  }).addTo(map);
+
+  L.control.zoom({ position: 'topright' }).addTo(map);
+}
+
+function initSVG() {
+  svg = d3.select(map.getPanes().overlayPane).append("svg");
+  g = svg.append("g").attr("class", "leaflet-zoom-hide");
+  transform = d3.geo.transform({ point: projectPoint });
+  d3path = d3.geo.path().projection(transform);
+}
+
+initMap();
+initSVG();
 
 function getLineFeature (trip, index) {
   var coordinates = L.PolylineUtil.decode(trip.direction);
@@ -73,6 +81,86 @@ function updateTimer () {
   timer = setTimeout(function () { updateTimer(); }, (1000 / timeFactor));
 }
 
+function animatePaths (rawData) {
+  g.selectAll('path').remove();
+  if (rawData.length === 0) return;
+
+  var startDate = getNYCTime(rawData[0].pickup_datetime)
+    , totalPaths = rawData.length, drawn = 0;
+
+  time = moment(startDate).zone('-05:00');
+  clearTimeout(timer);
+  updateTimer();
+
+  var data = {
+    type: 'FeatureCollection',
+    features: _.map(rawData, getLineFeature)
+  };
+
+  var feature = g.selectAll('path')
+    .data(data.features)
+    .enter().append('path')
+    .style('opacity', 0)
+    .attr('class', function (d) {
+      return ('trip-' + d.properties.key) + ' ' +
+             ('from-' + d.properties.terminal.slice(0, 3));
+    });
+
+  function transition (d, i) {
+    var path = this;
+    var marker = g.append('circle').attr('r', 3);
+
+    d3.select(path)
+      .style('opacity', .6)
+      .transition()
+      .duration(function (d) {
+        var duration = d.properties.duration;
+        return duration * 1000 / ( 60 * timeFactor);
+      })
+      .each('end', function (d) {
+        d3.select(this).remove();
+        drawn = drawn + 1;
+        // fetch for the next day
+      })
+      .attrTween('stroke-dasharray', function () {
+        var l = path.getTotalLength()
+          , i = d3.interpolateString('0,' + l, l + ',' + l);
+        return function (t) {
+          var p = path.getPointAtLength(t * l);
+          marker.attr('transform', 'translate(' + p.x + ',' + p.y + ')')
+          return i(t);
+        };
+      });
+  }
+
+  g.selectAll('path').each(function (d, i) {
+    var path = this, lastQueryTime = queryTime
+      , pickup = getNYCTime(d.properties.pickupTime)
+      , after = (pickup - startDate) / (60 * timeFactor);
+    setTimeout(function () {
+      if (lastQueryTime !== queryTime) return;
+      time = moment(pickup).zone('-05:00');
+      transition.call(path, d, i);
+    }, after);
+  });
+
+  map.on('viewreset', reset);
+  reset();
+
+  function reset () {
+    var bounds = d3path.bounds(data)
+      , topLeft = bounds[0]
+      , bottomRight = bounds[1];
+
+    svg.attr("width", bottomRight[0] - topLeft[0] + 100)
+      .attr("height", bottomRight[1] - topLeft[1] + 100)
+      .style("left", topLeft[0] - 50 + "px")
+      .style("top", topLeft[1] - 50 + "px");
+    g.attr("transform", "translate(" + (-topLeft[0]+50) + "," + (-topLeft[1]+50)+ ")");
+    feature.attr("d", d3path);
+  }
+}
+
 function updateQuery (query) {
   if (_.isUndefined(query)) query = {};
   var url = '/trip?' + $.param(query);
@@ -81,89 +169,7 @@ function updateQuery (query) {
   g.selectAll('path').transition(0);
   g.selectAll('circle').remove();
 
-  fetchNewData(url);
-}
-
-function fetchNewData (url) {
-  d3.json(url, function (rawData) {
-    g.selectAll('path').remove();
-    if (rawData.length === 0) return;
-
-    var startDate = getNYCTime(rawData[0].pickup_datetime)
-      , totalPaths = rawData.length, drawn = 0;
-
-    time = moment(startDate).zone('-05:00');
-    clearTimeout(timer);
-    updateTimer();
-
-    var data = {
-      type: 'FeatureCollection',
-      features: _.map(rawData, getLineFeature)
-    };
-
-    var feature = g.selectAll('path')
-      .data(data.features)
-      .enter().append('path')
-      .style('opacity', 0)
-      .attr('class', function (d) {
-        return ('trip-' + d.properties.key) + ' ' +
-               ('from-' + d.properties.terminal.slice(0, 3));
-      });
-
-    function transition (d, i) {
-      var path = this;
-      var marker = g.append('circle').attr('r', 3);
-
-      d3.select(path)
-        .style('opacity', .6)
-        .transition()
-        .duration(function (d) {
-          var duration = d.properties.duration;
-          return duration * 1000 / ( 60 * timeFactor);
-        })
-        .each('end', function (d) {
-          d3.select(this).remove();
-          drawn = drawn + 1;
-          // fetch for the next day
-        })
-        .attrTween('stroke-dasharray', function () {
-          var l = path.getTotalLength()
-            , i = d3.interpolateString('0,' + l, l + ',' + l);
-          return function (t) {
-            var p = path.getPointAtLength(t * l);
-            marker.attr('transform', 'translate(' + p.x + ',' + p.y + ')')
-            return i(t);
-          };
-        });
-    }
-
-    g.selectAll('path').each(function (d, i) {
-      var path = this, lastQueryTime = queryTime
-        , pickup = getNYCTime(d.properties.pickupTime)
-        , after = (pickup - startDate) / (60 * timeFactor);
-      setTimeout(function () {
-        if (lastQueryTime !== queryTime) return;
-        time = moment(pickup).zone('-05:00');
-        transition.call(path, d, i);
-      }, after);
-    });
-
-    map.on('viewreset', reset);
-    reset();
-
-    function reset () {
-      var bounds = d3path.bounds(data)
-        , topLeft = bounds[0]
-        , bottomRight = bounds[1];
-
-      svg.attr("width", bottomRight[0] - topLeft[0] + 100)
-        .attr("height", bottomRight[1] - topLeft[1] + 100)
-        .style("left", topLeft[0] - 50 + "px")
-        .style("top", topLeft[1] - 50 + "px");
-      g.attr("transform", "translate(" + (-topLeft[0]+50) + "," + (-topLeft[1]+50)+ ")");
-      feature.attr("d", d3path);
-    }
-  });
+  d3.json(url, animatePaths);
 }
 
 var startDate = '2013-12-22 00:00:00'
@@ -180,6 +186,8 @@ $(function () {
 
   $('.timeFactor').html(timeFactor);
 
+  $('#terminals').multiselect();
+
   $('.slower').click(function () {
     if (timeFactor > 1) timeFactor -= 1;
     $('.timeFactor').html(timeFactor);
@@ -189,8 +197,6 @@ $(function () {
     timeFactor += 1;
     $('.timeFactor').html(timeFactor);
   });
-
-  $('#terminals').multiselect();
 
   $('.filters .submit').click(function () {
     var terminals = $('#terminals').val();
